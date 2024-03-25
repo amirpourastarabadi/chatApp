fe161210e012235f053ab18ac8b7fd7bbe17963d
Amir Pourastarabadi <amir.pourastarabadi@shab.ir>
2024-03-25 21:07:49 +0330

    add chat.index livewire component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-500 ">
         <!-- Page Content -->
         <main>
             {{ $slot }}
