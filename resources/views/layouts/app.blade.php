a015231a7c8b04910838ed91a275271c6a2c3777
Amir Pourastarabadi <amir.pourastarabadi@shab.ir>
2024-03-26 21:54:40 +0330

    update chat-box component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-500 ">
+    <div class="min-h-screen bg-gray-100 ">
         <!-- Page Content -->
         <main>
             {{ $slot }}
--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -120,7 +120,24 @@
 
 
         {{-- send message  --}}
+        <footer class="shrink-0 z-10 bg-white inset-x-0">
 
+            <div class=" p-2 border-t">
+                <form method="POST" autocapitalize="off">
+                    @csrf
+
+                    <input type="hidden" autocomplete="false" style="display:none">
+
+                    <div class="grid grid-cols-12">
+                        <input type="text" autocomplete="off" autofocus placeholder="write your message here" maxlength="1700" class="col-span-10 bg-gray-100 border-0 outline-0 focus:border-0 focus:ring-0 hover:ring-0 rounded-lg  focus:outline-none">
+                        <button class="col-span-2" type='submit'>Send</button>
+                    </div>
+                </form>
+                @error('body')
+                <p> {{$message}} </p>
+                @enderror
+            </div>
+        </footer>
     </div>
 
 </div>
