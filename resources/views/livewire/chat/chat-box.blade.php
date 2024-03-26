c13eb11e598eb4919351776c255a3666600cb993
Amir Pourastarabadi <amir.pourastarabadi@shab.ir>
2024-03-26 21:27:21 +0330

    update chat-box header

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,3 +1,28 @@
-<div>
-    CHATBOX
+<div class="w-full overflow-hidden">
+
+    <div class="border-b flex flex-col overflow-y-scroll grow h-full">
+
+        {{-- header --}}
+        <header class="w-full sticky inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-white border-b ">
+            <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
+                <a class="shrink-0 lg:hidden" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                    </svg>
+                </a>
+
+                {{-- avatar --}}
+                <div class="shrink-0">
+                    <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
+                </div>
+                <h6 class="font-bold truncate"> {{fake()->email}} </h6>
+            </div>
+        </header>
+
+        {{-- body --}}
+
+        {{-- send message  --}}
+
+    </div>
+
 </div>
