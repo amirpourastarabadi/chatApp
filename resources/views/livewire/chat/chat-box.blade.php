5dd183d8d3bf564edf06a89a52c5d1a94d8ef078
Amir Pourastarabadi <amir.pourastarabadi@shab.ir>
2024-03-29 09:51:47 +0330

    fix chat-list click item

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -15,7 +15,7 @@
                 <div class="shrink-0">
                     <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
                 </div>
-                <h6 class="font-bold truncate"> {{fake()->email}} </h6>
+                <h6 class="font-bold truncate"> {{$selectedConversation?->reciver->name}} </h6>
             </div>
         </header>
 
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -51,11 +51,11 @@
 
             @foreach ($conversations as $conversation)
             <li id="conversation-{{ $conversation->id }}" wire:key="{{ $conversation->id }}" class="py-3 hover:bg-gray-50 rounded-2xl dark:hover:bg-gray-700/70 transition-colors duration-150 flex gap-4 relative w-full cursor-pointer px-2 {{$conversation->id == $selectedConversation->id ? 'bg-gray-400/70' : ''}}">
-                <a href="{{route('chats.show', ['chat' => $conversation->id])}}" class="shrink-0">
+                <a href="#" class="shrink-0">
                     <x-avatar src="https://source.unsplash.com/500x500?face-{{$conversation->reciver->id}}" />
                 </a>
                 <aside class="grid grid-cols-12 w-full">
-                    <a href="#" class="col-span-11 border-b pb-2 border-gray-200 relative overflow-hidden truncate leading-5 w-full flex-nowrap p-1">
+                    <a href="{{route('chats.show', ['chat' => $conversation->id])}}" class="col-span-11 border-b pb-2 border-gray-200 relative overflow-hidden truncate leading-5 w-full flex-nowrap p-1">
                         {{-- name and date  --}}
                         <div class="flex justify-between w-full items-center">
                             <h6 class="truncate font-medium tracking-wider text-gray-900">
